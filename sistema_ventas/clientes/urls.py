f3f5ff5d1ff80e49fce400fe6f478a94410a5cf5
soullesspy <maxgtmaster@hotmail.com>
2024-09-23 23:19:15 -0400

    agregado static, bootstrap

--- sistema_ventas/clientes/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.clientes_view, name='Clientes'),
-    ]
--- sistema_ventas/clientes/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def clientes_view(request):
-    return render(request, 'clientes.html')
