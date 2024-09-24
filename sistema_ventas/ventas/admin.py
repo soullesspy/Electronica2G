488d800504a40b0c25c79a20ad05d3be2fc4e021
soullesspy <maxgtmaster@hotmail.com>
2024-09-24 01:01:17 -0400

    model edit

--- sistema_ventas/ventas/admin.py
+++ sistema_ventas/ventas/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from ventas.models import Cliente, Producto
 
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'telefono','codigo', 'cedula_ruc')
+    search_fields = ['nombre']
+    readonly_fields = ('create', 'update')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Cliente, ClienteAdmin)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_producto', 'cantidad','codigo', 'costo')
+    search_fields = ['nombre_producto']
+    readonly_fields = ('create', 'update')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Producto, ProductoAdmin)
--- sistema_ventas/ventas/models.py
+++ sistema_ventas/ventas/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente (models.Model):
+    codigo = models.CharField(max_length=200, unique=True, null=True, blank=True)
+    nombre = models.CharField(max_length=255, null=True, blank=True)
+    direccion = models.CharField(max_length=255, null=True, blank=True)
+    cedula_ruc = models.CharField(max_length=10, null=True, blank=True)
+    telefono = models.CharField(max_length=10, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    create = models.DateField(auto_now_add=True)
+    update = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'cliente'
+        verbose_name_plural = 'Clientes'
+    
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    codigo = models.CharField(max_length=200, unique=True, null=True, blank=True)
+    nombre_producto = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    descripcion = models.CharField(max_length=255, unique=True, null=False)
+    imagen = models.ImageField(upload_to='productos', null=True, blank=True)
+    costo = models.DecimalField(max_digits=15, decimal_places=2, null=False)
+    cantidad = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    create = models.DateField(auto_now_add=True)
+    update = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'producto'
+        verbose_name_plural = 'Productos'
+        order_with_respect_to = 'nombre_producto'
+    
+    def __str__(self):
+        return self.nombre_product
+
+
