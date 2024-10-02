7a1e6791e029df19c780d00bebf80cf0d863657b
soullesspy <maxgtmaster@hotmail.com>
2024-10-01 20:02:10 -0400

    fix edit

--- sistema_ventas/ventas/urls.py
+++ sistema_ventas/ventas/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.ventas_view, name='Ventas'),
     path('clientes/',views.clientes_view, name='Clientes'),
     path('add_cliente/',views.add_cliente_view, name='AddCliente'),
-    path('add_cliente/',views.edit_cliente_view, name='EditCliente'),
+    path('edit_cliente/',views.edit_cliente_view, name='EditCliente'),
     path('delete_cliente/',views.delete_cliente_view, name='DeleteCliente'),
     ]
