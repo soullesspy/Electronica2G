82d68be9e784f997f58c6300949144fe7cd7f116
soullesspy <maxgtmaster@hotmail.com>
2024-10-08 19:50:49 -0300

    cambio de DB a mysql

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # inicializar django
 cd /sistema_ventas
-python manage.py makemigrations
+#python manage.py makemigrations
 python manage.py migrate
+python manage.py createsuperuser
 superuser admin:administrador1.
--- sistema_ventas/sistema_ventas/settings.py
+++ sistema_ventas/sistema_ventas/settings.py
@@ -75,11 +75,18 @@ WSGI_APPLICATION = 'sistema_ventas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+#lineas utilizando sqlite3
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistema_ventas_DB',
+        'USER': 'sistema_ventas_app',
+        'PASSWORD': 'SisVent24.',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
